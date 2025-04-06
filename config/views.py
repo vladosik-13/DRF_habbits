f7f03aa6b5beafeb57fd6a7ea8c8a919496a8851
Владислав Кошелев <testvlad59@gmail.com>
2025-04-07 00:18:47 +0300

    код исправлен с помощью линтера flake8

--- config/views.py
+++ config/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from telegram_bot.serializers import RegisterSerializer, UserSerializer
 
